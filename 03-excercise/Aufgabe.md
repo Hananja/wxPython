12cf3248cbf235e80520e8ebfba3775949ec4c84
hananja <e.loos@schule.bremen.de>
2019-11-29 15:15:58 +0100

    aktualisiert

--- 03-excercise/Aufgabe.md
+++ 03-excercise/Aufgabe.md
@@ -1,4 +1,8 @@
-Erweitern Sie Ihre Hello World Anwendung so, dass
+1. Erstellen Sie eine Hello World Anwendung mit Python und wxFormBuilder, die beim Klick auf einen Button `Hello World` 
+auf der Konsole ausgibt.
 
-* eine MessageBox mit der Begrüßung ausgegeben wird und
-* im Hauptfenster ein Name eingegeben werden kann, der für die Begüßung verwendet wird.
+2. Erweitern Sie Ihre Hello World Anwendung so, dass
+    
+    * eine MessageBox mit der Begrüßung ausgegeben wird und
+    
+    * im Hauptfenster ein Name eingegeben werden kann, der für die Begüßung verwendet wird.
--- 03-excercise/generated.py
+++ 03-excercise/generated.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ###########################################################################
-## Python code generated with wxFormBuilder (version Nov 15 2019)
+## Python code generated with wxFormBuilder (version Oct 26 2018)
 ## http://www.wxformbuilder.org/
 ##
 ## PLEASE DO *NOT* EDIT THIS FILE!
@@ -16,21 +16,26 @@ import wx.xrc
 
 class AbstractMainPanel ( wx.Panel ):
 
-	def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
+	def __init__( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 605,300 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
 		wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
 
-		main_sizer = wx.BoxSizer( wx.HORIZONTAL )
+		main_sizer = wx.BoxSizer( wx.VERTICAL )
 
-		self.input_label = wx.StaticText( self, wx.ID_ANY, u"Name:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		sub_sizer = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.input_label = wx.StaticText( self, wx.ID_ANY, u"Name:", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_RIGHT )
 		self.input_label.Wrap( -1 )
 
-		main_sizer.Add( self.input_label, 0, wx.ALL, 5 )
+		sub_sizer.Add( self.input_label, 1, wx.ALL, 5 )
 
 		self.name_input = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_PROCESS_ENTER )
-		main_sizer.Add( self.name_input, 1, wx.ALL, 5 )
+		sub_sizer.Add( self.name_input, 2, wx.ALL, 5 )
 
 		self.hello_button = wx.Button( self, wx.ID_ANY, u"Go", wx.DefaultPosition, wx.DefaultSize, 0 )
-		main_sizer.Add( self.hello_button, 0, wx.ALL, 5 )
+		sub_sizer.Add( self.hello_button, 1, wx.ALL, 5 )
+
+
+		main_sizer.Add( sub_sizer, 0, wx.EXPAND, 5 )
 
 
 		self.SetSizer( main_sizer )
